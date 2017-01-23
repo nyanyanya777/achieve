f610a2ebb20d480bd7b4030e1e8b27cc0d1e6eb1
Kohei <yuryuri1477@gmail.com>
2017-01-23 06:17:29 +0000

    7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 
-has_many :blog
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+         has_many :blogs
 end
