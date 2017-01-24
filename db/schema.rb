ddd4b0fff2ecfbf22533829bfbcfee14aebe9077
Kohei <yuryuri1477@gmail.com>
2017-01-24 01:58:43 +0000

    7

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123103807) do
+ActiveRecord::Schema.define(version: 20170124015653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20170123103807) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.string   "name"
-    t.string   "user_name"
   end
 
   create_table "contacts", force: :cascade do |t|
