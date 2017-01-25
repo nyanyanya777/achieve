9a183a301f4edb2bc041bdd0725b44bdabab8b59
Kohei <yuryuri1477@gmail.com>
2017-01-25 07:51:36 +0000

    7

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-config.action_mailer.default_url_options = { host: 'fathomless-waters-15357.herokuapp.como' }
+config.action_mailer.default_url_options = { host: 'fathomless-waters-15357.herokuapp.com' }
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings =
 {
