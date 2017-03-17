63615157ece983bfbb8892a2b610c1d583e50386
kohei <kohei@koheinoMacBook-Air.local>
2017-03-17 14:30:52 +0900

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,8 +36,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  http://52.192.110.253/
-config.action_mailer.default_url_options = { host: 'http://52.192.110.253//' }
+config.action_mailer.default_url_options = { host: '52.192.110.253' }
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings =
 {
